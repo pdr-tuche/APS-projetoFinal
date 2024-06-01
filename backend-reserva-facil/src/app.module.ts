c4e6e00b3db14b274bc0004ed0c8a39ba9511701
Pedro Neves <contato.nevespedro@gmail.com>
2024-05-31 22:12:41 -0300

    criado modulo user

--- backend-reserva-facil/src/app.module.ts
+++ backend-reserva-facil/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
+import { UserService } from './user/user.service';
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, UserModule],
+  controllers: [UserModule],
+  providers: [UserService],
 })
 export class AppModule {}
