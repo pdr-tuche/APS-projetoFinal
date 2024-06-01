fe54367d565beb1672f0f3100f3ed56518aacfd7
Pedro <contato.nevespedro@gmail.com>
2024-06-01 00:01:58 -0300

    retirado erro
    Nest can't resolve dependencies of the ItemsService (?). Please make sure that the argument at index [0] is available in the AppModule context

--- backend-reserva-facil/src/app.module.ts
+++ backend-reserva-facil/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
+import { UserController } from './user/user.controller';
 
 @Module({
-  imports: [PrismaModule, UserModule],
-  controllers: [UserModule],
+  imports: [PrismaModule],
+  controllers: [UserController],
   providers: [UserService],
 })
 export class AppModule {}
