f200eda9d98f3669fa9c39ec117789bef9b9d767
Pedro Neves <contato.nevespedro@gmail.com>
2024-06-04 19:48:21 -0300

    fix: rota create publica

--- backend-reserva-facil/src/user/user.controller.ts
+++ backend-reserva-facil/src/user/user.controller.ts
@@ -10,12 +10,14 @@ import {
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
+import { IsPublic } from 'src/auth/decorators/is-public.decorator';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Post()
+  @IsPublic()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
